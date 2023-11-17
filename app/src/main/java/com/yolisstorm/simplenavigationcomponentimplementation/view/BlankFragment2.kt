3ac9253f6ac258815aa1c32a103e2878fe3680d1
EliseevR <VTB4046473@corp.dev.vtb>
2023-11-17 16:01:58 +0300

    Initial commit

---
+++ app/src/main/java/com/yolisstorm/simplenavigationcomponentimplementation/view/BlankFragment2.kt
@@ -0,0 +1,33 @@
+package com.yolisstorm.simplenavigationcomponentimplementation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import com.yolisstorm.simplenavigationcomponentimplementation.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment2.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank_2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.navigate_button).setOnClickListener {
+            view.findNavController().navigate(R.id.action_blankFragment2_to_blankFragment3)
+        }
+    }
+}
